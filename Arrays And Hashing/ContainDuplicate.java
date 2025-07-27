aa96b98151c00620afa8eed17d42e5ce05b19676
Shrutik Vankundre <shrutikvankundre1008@gmail.com>
2025-07-27 20:29:22 +0530

    Add initial implementation of ContainDuplicate and hello classes

---
+++ Arrays And Hashing/ContainDuplicate.java
@@ -0,0 +1,20 @@
+
+import java.util.HashSet;
+
+public class ContainDuplicate{
+    public static void main(String[] args) {
+
+        int[] a ={1,2,3,4,5,5,5,6,6};
+        remove(a);
+    }
+    public static void remove(int[] a){
+           HashSet<Integer> s = new HashSet<>();
+            int n= a.length;
+        for(int i=0;i<n;i++){
+            s.add(a[i]);
+        }
+
+        System.out.println(s);
+    }
+
+}
