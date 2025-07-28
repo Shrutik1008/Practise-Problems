0cbc0a0d1aa1f6cde2a099010bae027abcd1013a
Shrutik Vankundre <shrutikvankundre1008@gmail.com>
2025-07-28 08:44:50 +0530

    new problems

---
+++ Arrays And Hashing/Anagram.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Anagram {
+    public static void Anagrams(String Str1,String Str2){
+        if(Str1.length() !=Str2.length()){
+            System.out.println("Anagrams cannot be found");
+            return;
+        }
+
+        char[] S1 = Str1.toCharArray();
+        char[] S2 = Str2.toCharArray();
+        Arrays.sort(S1);
+        Arrays.sort(S2);
+        if(Arrays.equals(S1,S2)){
+            System.out.println("Anagrams found");
+        }else{
+            System.out.println("Anagrams cannot be found");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String Str1 = "listn";
+        String Str2 = "silent";
+        Anagrams(Str1, Str2);
+    }
+}
--- Arrays And Hashing/ContainDuplicate.java
+++ Arrays And Hashing/ContainDuplicate.java
@@ -1,6 +1,4 @@
-
 import java.util.HashSet;
-
 public class ContainDuplicate{
     public static void main(String[] args) {
 
